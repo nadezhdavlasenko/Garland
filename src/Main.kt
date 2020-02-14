1429cdaaecee5fc25f546362593b36c41fcfd6fa
Nadiia Vlasenko <ciklum.vlasenko@extaccount.com>
2020-02-14 21:21:05 +0200

    smth

--- src/Main.kt
+++ src/Main.kt
@@ -2,10 +2,18 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() {
-    val reader: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val reader = BufferedReader(InputStreamReader(System.`in`))
      val list = reader.readLine().split(" ")
     val numberOfLamps = list[0].toInt()
     val leftmostHeight = list[1].toDouble()
     println("numberOfLamps=$numberOfLamps")
     print(("leftmostHeight=$leftmostHeight"))
+}
+
+fun findRightBorder(numberOfLamps: Int, leftmostHeight: Double) {
+    val bad = -1
+    var rightMostHeight = Double.MAX_VALUE
+    for (i in 0 until numberOfLamps) {
+        val height = 
+    }
 }
